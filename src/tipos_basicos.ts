d9240bbf3e4134406e5a57e5c7a31460fc29d91e
maatheOoz <matheussantosbmth@gmail.com>
2024-07-14 22:27:02 -0300

    commit inicial

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,26 @@
+let estaChovendo: boolean = false
+
+estaChovendo = true
+
+let idade: number = 25
+
+let altura: number = 1.70
+
+const nacionalidade: string = 'Brasileiro'
+
+const colegas: string[] = ['Lucas', 'Carlos']
+const tecnologias: Array<string> = ['html', 'css', 'js']
+const notas: ReadonlyArray<number> = [7, 9, 5, 8]
+
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['Matheus', true, 25]
+
+let idadeDaAna: number | string = 25
+idadeDaAna = '25 anos'
+
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1, 2, 3]
+dadosDaApi = 'string'
+
+let curso = 'front-end'
